d1a1635e1860569bf510f553c5a44fda0dd484a6
namudongs <85597534+namudongs@users.noreply.github.com>
2023-07-29 03:58:16 +0900

    [add] 로그인, 회원가입 화면 TextField unfocus 기능 구현

--- lib/authentication/login_page.dart
+++ lib/authentication/login_page.dart
@@ -16,141 +16,149 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            size: 20,
-            color: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            ),
           ),
+          systemOverlayStyle: SystemUiOverlayStyle.light,
         ),
-        systemOverlayStyle: SystemUiOverlayStyle.light,
-      ),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        const Text(
-                          "로그인",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "재학중인 학교의 이메일로 로그인하세요.",
-                          style:
-                              TextStyle(fontSize: 15, color: Colors.grey[700]),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 40),
-                      child: Column(
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
                         children: <Widget>[
-                          makeInput(label: "이메일", controller: emailController),
-                          makeInput(
-                            label: "비밀번호",
-                            obscureText: true,
-                            controller: passwordController,
+                          const Text(
+                            "로그인",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "재학중인 학교의 이메일로 로그인하세요.",
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[700]),
                           ),
                         ],
                       ),
-                    ),
-                    Padding(
+                      Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 40),
-                        child: ColorRoundButton(
-                            tapFunc: () {
-                              String email = emailController.text;
-                              String password = passwordController.text;
-
-                              if (email.isEmpty || password.isEmpty) {
-                                print('이메일 또는 비밀번호를 입력해주세요.');
-                                return;
-                              } else if (!email.contains('@')) {
-                                print('이메일 형식이 올바르지 않습니다.');
-                                return;
-                              } else if (!email.contains('.ac.kr')) {
-                                print('재학중인 학교의 이메일을 입력해 주세요.');
-                                return;
-                              } else if (password.length < 6) {
-                                print('비밀번호는 6자리 이상이어야 합니다.');
-                                return;
-                              } else if (password.contains(' ')) {
-                                print('비밀번호에 공백이 포함되어 있습니다.');
-                                return;
-                              } else {
-                                FirebaseAuth.instance
-                                    .signInWithEmailAndPassword(
-                                        email: email, password: password)
-                                    .then((value) {
-                                  print('로그인 성공\n이메일: $email, 비밀번호: $password');
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              const BottomNavBar()));
-                                }).catchError((e) {
-                                  print('로그인 실패\n이메일: $email, 비밀번호: $password');
-                                  print(e);
-                                });
-                              }
-                            },
-                            title: "로그인",
-                            color: Colors.lightBlueAccent,
-                            buttonWidth: double.infinity,
-                            buttonHeight: 60,
-                            fontSize: 18)),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          const Text("아직 계정이 없으신가요? "),
-                          GestureDetector(
-                            onTap: () {
-                              print('회원가입 버튼 클릭');
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => SignupPage()));
-                            },
-                            child: const Text(
-                              "회원가입",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  decoration: TextDecoration.underline),
+                        child: Column(
+                          children: <Widget>[
+                            makeInput(
+                                label: "이메일", controller: emailController),
+                            makeInput(
+                              label: "비밀번호",
+                              obscureText: true,
+                              controller: passwordController,
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    )
-                  ],
+                      Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: ColorRoundButton(
+                              tapFunc: () {
+                                String email = emailController.text;
+                                String password = passwordController.text;
+
+                                if (email.isEmpty || password.isEmpty) {
+                                  print('이메일 또는 비밀번호를 입력해주세요.');
+                                  return;
+                                } else if (!email.contains('@')) {
+                                  print('이메일 형식이 올바르지 않습니다.');
+                                  return;
+                                } else if (!email.contains('.ac.kr')) {
+                                  print('재학중인 학교의 이메일을 입력해 주세요.');
+                                  return;
+                                } else if (password.length < 6) {
+                                  print('비밀번호는 6자리 이상이어야 합니다.');
+                                  return;
+                                } else if (password.contains(' ')) {
+                                  print('비밀번호에 공백이 포함되어 있습니다.');
+                                  return;
+                                } else {
+                                  FirebaseAuth.instance
+                                      .signInWithEmailAndPassword(
+                                          email: email, password: password)
+                                      .then((value) {
+                                    print(
+                                        '로그인 성공\n이메일: $email, 비밀번호: $password');
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const BottomNavBar()));
+                                  }).catchError((e) {
+                                    print(
+                                        '로그인 실패\n이메일: $email, 비밀번호: $password');
+                                    print(e);
+                                  });
+                                }
+                              },
+                              title: "로그인",
+                              color: Colors.lightBlueAccent,
+                              buttonWidth: double.infinity,
+                              buttonHeight: 60,
+                              fontSize: 18)),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Text("아직 계정이 없으신가요? "),
+                            GestureDetector(
+                              onTap: () {
+                                print('회원가입 버튼 클릭');
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => SignupPage()));
+                              },
+                              child: const Text(
+                                "회원가입",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    decoration: TextDecoration.underline),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                  height: MediaQuery.of(context).size.height / 3,
-                  child: Lottie.asset(
-                    'assets/lottie/animation_lkij23o3.json',
-                  ))
-            ],
+                SizedBox(
+                    height: MediaQuery.of(context).size.height / 3,
+                    child: Lottie.asset(
+                      'assets/lottie/animation_lkij23o3.json',
+                    ))
+              ],
+            ),
           ),
         ),
       ),
--- lib/authentication/signup_page.dart
+++ lib/authentication/signup_page.dart
@@ -15,132 +15,138 @@ class SignupPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            size: 20,
-            color: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            ),
           ),
+          systemOverlayStyle: SystemUiOverlayStyle.light,
         ),
-        systemOverlayStyle: SystemUiOverlayStyle.light,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          height: MediaQuery.of(context).size.height - 150,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  const Text(
-                    "회원가입",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    "재학중인 학교의 이메일로 회원가입할 수 있습니다.",
-                    style: TextStyle(fontSize: 14, color: Colors.grey[700]),
-                  ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  makeInput(label: "이메일", controller: emailController),
-                  makeInput(
-                      label: "비밀번호",
-                      obscureText: true,
-                      controller: passwordController),
-                  makeInput(
-                      label: "비밀번호 확인",
-                      obscureText: true,
-                      controller: pwconfirmController),
-                ],
-              ),
-              ColorRoundButton(
-                  tapFunc: () {
-                    String email = emailController.text;
-                    String password = passwordController.text;
-                    String pwconfirm = pwconfirmController.text;
-
-                    if (email.isEmpty ||
-                        password.isEmpty ||
-                        pwconfirm.isEmpty) {
-                      print('빈칸이 있습니다.');
-                      return;
-                    } else if (!email.contains('@')) {
-                      print('이메일 형식이 올바르지 않습니다.');
-                      return;
-                    } else if (!email.contains('.ac.kr')) {
-                      print('재학중인 학교의 이메일을 입력해 주세요.');
-                      return;
-                    } else if (password.length < 6) {
-                      print('비밀번호는 6자리 이상이어야 합니다.');
-                      return;
-                    } else if (password.contains(' ') &&
-                        pwconfirm.contains(' ')) {
-                      print('비밀번호에 공백이 포함되어 있습니다.');
-                      return;
-                    } else if (password != pwconfirm) {
-                      print('비밀번호가 일치하지 않습니다.');
-                      return;
-                    } else {
-                      print('회원가입 버튼 클릭');
-                      FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                              email: email, password: password)
-                          .then((value) {
-                        print('회원가입 성공');
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const BottomNavBar()));
-                      }).catchError((err) {
-                        print(err);
-                      });
-                    }
-                  },
-                  title: "회원가입",
-                  color: Colors.blue,
-                  buttonWidth: double.infinity,
-                  buttonHeight: 60,
-                  fontSize: 18),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            height: MediaQuery.of(context).size.height - 150,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
                   children: <Widget>[
-                    const Text("이미 계정이 있으신가요? "),
-                    GestureDetector(
-                      onTap: () {
-                        print('로그인 버튼 클릭');
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginPage()));
-                      },
-                      child: const Text(
-                        "로그인",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline),
-                      ),
+                    const Text(
+                      "회원가입",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "재학중인 학교의 이메일로 회원가입할 수 있습니다.",
+                      style: TextStyle(fontSize: 14, color: Colors.grey[700]),
                     ),
                   ],
                 ),
-              ),
-            ],
+                Column(
+                  children: <Widget>[
+                    makeInput(label: "이메일", controller: emailController),
+                    makeInput(
+                        label: "비밀번호",
+                        obscureText: true,
+                        controller: passwordController),
+                    makeInput(
+                        label: "비밀번호 확인",
+                        obscureText: true,
+                        controller: pwconfirmController),
+                  ],
+                ),
+                ColorRoundButton(
+                    tapFunc: () {
+                      String email = emailController.text;
+                      String password = passwordController.text;
+                      String pwconfirm = pwconfirmController.text;
+
+                      if (email.isEmpty ||
+                          password.isEmpty ||
+                          pwconfirm.isEmpty) {
+                        print('빈칸이 있습니다.');
+                        return;
+                      } else if (!email.contains('@')) {
+                        print('이메일 형식이 올바르지 않습니다.');
+                        return;
+                      } else if (!email.contains('.ac.kr')) {
+                        print('재학중인 학교의 이메일을 입력해 주세요.');
+                        return;
+                      } else if (password.length < 6) {
+                        print('비밀번호는 6자리 이상이어야 합니다.');
+                        return;
+                      } else if (password.contains(' ') &&
+                          pwconfirm.contains(' ')) {
+                        print('비밀번호에 공백이 포함되어 있습니다.');
+                        return;
+                      } else if (password != pwconfirm) {
+                        print('비밀번호가 일치하지 않습니다.');
+                        return;
+                      } else {
+                        print('회원가입 버튼 클릭');
+                        FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: email, password: password)
+                            .then((value) {
+                          print('회원가입 성공');
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const BottomNavBar()));
+                        }).catchError((err) {
+                          print(err);
+                        });
+                      }
+                    },
+                    title: "회원가입",
+                    color: Colors.blue,
+                    buttonWidth: double.infinity,
+                    buttonHeight: 60,
+                    fontSize: 18),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text("이미 계정이 있으신가요? "),
+                      GestureDetector(
+                        onTap: () {
+                          print('로그인 버튼 클릭');
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()));
+                        },
+                        child: const Text(
+                          "로그인",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
