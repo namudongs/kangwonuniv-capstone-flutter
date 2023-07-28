0da4f5f0928f88e142e0462a775c7a1281fa429d
namudongs <85597534+namudongs@users.noreply.github.com>
2023-07-29 03:59:21 +0900

    [fix] RenderFlex overflow 오류 해결

--- lib/authentication/main_page.dart
+++ lib/authentication/main_page.dart
@@ -11,64 +11,66 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                const Text(
-                  "환영합니다!",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "실시간 질의응답으로 대학생활을 더 즐겁게",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.grey[700], fontSize: 15),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 2,
-              child: Lottie.asset('assets/lottie/animation_lkiiozif.json'),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              child: Column(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
                 children: <Widget>[
-                  ColorRoundButton(
-                      tapFunc: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginPage()));
-                      },
-                      title: "로그인",
-                      color: Colors.white,
-                      buttonWidth: double.infinity,
-                      buttonHeight: 60,
-                      fontSize: 18),
+                  const Text(
+                    "환영합니다!",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
                   const SizedBox(
                     height: 10,
                   ),
-                  ColorRoundButton(
-                      tapFunc: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SignupPage()));
-                      },
-                      title: "회원가입",
-                      color: Colors.lightBlue,
-                      buttonWidth: double.infinity,
-                      buttonHeight: 60,
-                      fontSize: 18)
+                  Text(
+                    "실시간 질의응답으로 대학생활을 더 즐겁게",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.grey[700], fontSize: 15),
+                  ),
                 ],
               ),
-            )
-          ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 2,
+                child: Lottie.asset('assets/lottie/animation_lkiiozif.json'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  children: <Widget>[
+                    ColorRoundButton(
+                        tapFunc: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()));
+                        },
+                        title: "로그인",
+                        color: Colors.white,
+                        buttonWidth: double.infinity,
+                        buttonHeight: 60,
+                        fontSize: 18),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ColorRoundButton(
+                        tapFunc: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignupPage()));
+                        },
+                        title: "회원가입",
+                        color: Colors.lightBlue,
+                        buttonWidth: double.infinity,
+                        buttonHeight: 60,
+                        fontSize: 18)
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
