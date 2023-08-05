e4798bb92886bc703e510e5e8a3973e12e55dbb9
namudongs <85597534+namudongs@users.noreply.github.com>
2023-08-05 14:23:57 +0900

    [feat] 시간표 UI

--- lib/timetable/time_page.dart
+++ lib/timetable/time_page.dart
@@ -1,18 +1,116 @@
 import 'package:flutter/material.dart';
 
-class TimePage extends StatelessWidget {
+class TimePage extends StatefulWidget {
   const TimePage({super.key});
 
   @override
+  State<TimePage> createState() => _TimePageState();
+}
+
+class _TimePageState extends State<TimePage> {
+  List week = ['월', '화', '수', '목', '금'];
+  var kColumnLength = 22;
+  double kFirstColumnHeight = 20;
+  double kBoxSize = 52;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "시간표 페이지 만들어서 연결하기",
-          style: TextStyle(fontSize: 15),
-          textAlign: TextAlign.center,
+    return Scaffold(
+        body: Center(
+      child: Container(
+        width: kBoxSize * 7 + 15,
+        height: kColumnLength / 2 * kBoxSize + kColumnLength,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            // 첫 번째 열에는 시간표가 들어갑니다.
+            Column(
+              children: [
+                buildTimeColumn(),
+              ],
+            ),
+
+            // 두 번째 열에는 요일(Column)이 들어갑니다.
+            // buildDayColumn 메서드를 호출하여 요일(Column)을 생성합니다.
+            ...buildDayColumn(0), // 월요일
+            ...buildDayColumn(1), // 화요일
+            ...buildDayColumn(2), // 수요일
+            ...buildDayColumn(3), // 목요일
+            ...buildDayColumn(4), // 금요일
+          ],
         ),
       ),
+    ));
+  }
+
+  Expanded buildTimeColumn() {
+    return Expanded(
+      child: Column(
+        children: [
+          SizedBox(
+            height: kFirstColumnHeight,
+          ),
+          ...List.generate(
+            kColumnLength,
+            (index) {
+              if (index % 2 == 0) {
+                return const Divider(
+                  color: Colors.grey,
+                  height: 0,
+                );
+              }
+              return SizedBox(
+                height: kBoxSize,
+                child: Center(child: Text('${index ~/ 2 + 9}')),
+              );
+            },
+          ),
+        ],
+      ),
     );
+  }
+
+  List<Widget> buildDayColumn(int index) {
+    return [
+      const VerticalDivider(
+        color: Colors.grey,
+        width: 0,
+      ),
+      Expanded(
+        flex: 4,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                  child: Text(
+                    '${week[index]}',
+                  ),
+                ),
+                ...List.generate(
+                  kColumnLength,
+                  (index) {
+                    if (index % 2 == 0) {
+                      return const Divider(
+                        color: Colors.grey,
+                        height: 0,
+                      );
+                    }
+                    return SizedBox(
+                      height: kBoxSize,
+                      child: Container(),
+                    );
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ];
   }
 }
